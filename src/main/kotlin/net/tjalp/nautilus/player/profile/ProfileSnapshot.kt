189911cdffab726cda1d2ba141ded42ae23c46a3
tjalp <60233996+tjalp@users.noreply.github.com>
2022-10-02 13:39:46 +0200

    feat: add profile saving

--- src/main/kotlin/net/tjalp/nautilus/player/profile/ProfileSnapshot.kt
+++ src/main/kotlin/net/tjalp/nautilus/player/profile/ProfileSnapshot.kt
@@ -1,6 +1,10 @@
 package net.tjalp.nautilus.player.profile
 
+import kotlinx.coroutines.reactive.awaitSingle
+import net.tjalp.nautilus.Nautilus
 import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.reactivestreams.getCollectionOfName
+import org.litote.kmongo.reactivestreams.save
 import java.util.*
 
 /**
@@ -11,4 +15,13 @@ import java.util.*
 data class ProfileSnapshot(
     @BsonId val uniqueId: UUID,
     var data: String = ""
-)
+) {
+
+    private val nautilus = Nautilus.get()
+    private val profiles = this.nautilus.mongo.mongoDatabase.getCollectionOfName<ProfileSnapshot>("profiles")
+
+    /**
+     * Save this [ProfileSnapshot] to the database
+     */
+    suspend fun save() = this.profiles.save(this).awaitSingle()
+}
